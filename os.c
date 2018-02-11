42a8076114d2f1a6518c047852eb19470e3fe941
Justin <justintomas783@gmail.com>
2018-02-10 17:18:16 -0800

    Added second timer and updated start_system_timer();

--- os.c
+++ os.c
@@ -54,6 +54,27 @@ ISR(TIMER0_COMPA_vect) {
    //and r0 before exiting the ISR
 }
 
+ISR(TIMER1_COMPA_vect) {
+   //This interrupt routine is run once a second
+   //The 2 interrupt routines will not interrupt each other
+}
+
+void start_system_timer() {
+   //start timer 0 for OS system interrupt
+   TIMSK0 |= _BV(OCIE0A);             //interrupt on compare match
+   TCCR0A |= _BV(WGM01);              //clear timer on compare match
+
+   //Generate timer interrupt every ~10 milliseconds
+   TCCR0B |= _BV(CS02) | _BV(CS00);   //prescalar /1024
+   OCR0A = 156;                       //generate interrupt every 9.98 milliseconds
+                                      //start timer 1 to generate interrupt every 1 second
+   OCR1A = 15625;
+   TIMSK1 |= _BV(OCIE1A);             //interrupt on compare
+   TCCR1B |= _BV(WGM12) | _BV(CS12) | _BV(CS10); //slowest prescalar /1024
+
+}
+
+/* Legacy Code
 //Call this to start the system timer interrupt
 void start_system_timer() {
    TIMSK0 |= _BV(OCIE0A);  //interrupt on compare match
@@ -63,6 +84,8 @@ void start_system_timer() {
    TCCR0B |= _BV(CS02) | _BV(CS00);    //prescalar /1024
    OCR0A = 156;             //generate interrupt every 9.98 milliseconds
 }
+*/
+
 __attribute__((naked)) void context_switch(uint16_t *new_tp, uint16_t *old_tp) {
    asm volatile ("push r2");
    asm volatile ("push r3");
